18d506fc3715b92b5bde78dff2e8b8824ae01e6d
unknown <prajapatijonish@gmail.com>
2023-06-06 11:20:22 +0530

    numbers work practices

---
+++ Week_1/Day_1/numbers/numbers.py
@@ -0,0 +1,45 @@
+#formula of area of rectangle is l*b
+#solution 1
+'''
+x=92
+y = 48.8
+print(f"Total area of football field of {x}*{y} is ", x*y)
+'''
+
+#solution 2
+'''
+totalPackets = 9
+eachPacketCosts = 1.49
+paid=20
+c =totalPackets*eachPacketCosts
+print(f"The total cost of {totalPackets} packets is ", c,"$")
+x = paid-c
+print("Shopkeeper gave me: ", x,"$")
+'''
+
+#solution 3
+#area of square = 3.14*r*r
+'''
+length = 5.5
+tilesCost = 500
+
+totalFeet = 5.5**2
+print(f"The total area of tiles of {length} is ", totalFeet)
+c = tilesCost * totalFeet
+print("Total Price is ", c,"Rs")
+'''
+
+#binary =>10001
+
+'''
+decimalNumber=17
+a = 17%2
+b = 8%2
+c = 4%2
+d = 2%2
+e = 1%2
+
+print("The binary of 17 is ",str(a) + str(b) + str(c) + str(d) + str(e))
+
+print("Binary of number 17 is: ", format(decimalNumber,'b'))
+'''
