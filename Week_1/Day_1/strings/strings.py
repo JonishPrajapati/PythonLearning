a4e740a7972cf7da9c8c9425f3684f3f71ecb586
unknown <prajapatijonish@gmail.com>
2023-06-06 11:46:18 +0530

    completed string practice session

---
+++ Week_1/Day_1/strings/strings.py
@@ -0,0 +1,31 @@
+#solution 1
+'''
+street = input("Enter the street name: ")
+city = input("Enter the city name: ")
+country = input("Enter the country name: ")
+
+address = street +" " + city + " " +  country
+print("the address is: ", address)
+print(f"street: {street} \n city: {city} \n country: {country} ")
+'''
+
+#solution 2
+'''
+x = "Earth revolves around the sun"
+y = x[6:14]
+print(y)
+print(x[-3:])
+'''
+
+#solution 3
+'''
+x = "apple"
+y = "potoato"
+
+print(f"I eat {y} veggies and {x} fruits daily")
+'''
+
+#solution 4
+s = "maile 200 wota kera khae"
+newSentence = s.replace("200","10").replace("kera", "samosa")
+print(newSentence)
