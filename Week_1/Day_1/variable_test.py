85c09c36e0fa9325b15e34a489ea623019930aa8
unknown <prajapatijonish@gmail.com>
2023-06-06 10:51:23 +0530

    solved basic python variables question

---
+++ Week_1/Day_1/variable_test.py
@@ -0,0 +1,37 @@
+#solution 1
+# break = 5
+# print(break)
+
+'''
+break can be used only within a loop because it is python keyword (reserved words)
+'''
+#solution 2
+'''
+x= int(input("Your birth year: "))
+y = 2023
+
+c = y-x
+print("Your current age is : ", c)
+'''
+
+
+#solution 3
+'''
+x= input("Firstname: ")
+y = input("Middlename: ")
+z = input("Lastname: ")
+
+print(f"Your full name is {x} {y} {z}.")
+'''
+
+#solution 4
+
+'''
+_nation = 1 #right
+1record=2
+record1=3 #right
+record_one=4
+record-one=5
+record^one =6
+#Python is dynamically typed varialble.
+'''
